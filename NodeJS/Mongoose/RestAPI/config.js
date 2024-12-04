658908c4fd8e2c3b5d8ce2948cfe7d67d258a43e
shivanij-1511 <153703668+shivanij-1511@users.noreply.github.com>
2024-12-04 13:30:41 +0530

    Delete NodeJS/Mongoose directory

--- NodeJS/Mongoose/RestAPI/config.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose')
-
-const con=mongoose.connect("mongodb://localhost:27017/CRUD")
-if(con){
-    console.log("DB connected successfully");
-}
-else{
-    console.log("db unable to connect");
-}
